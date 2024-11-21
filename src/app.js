7dabb8ddba2e73a406a0851825d994589c95fdbb
arpitmalviya26 <malviyaarpit202@gmail.com>
2024-11-22 01:35:03 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express(); //creating our webserver 
+
+app.use("/test" , (req,res)=>{
+    res.send("Hello From the server");
+})
+
+app.use("/test2" , (req,res)=>{
+    res.send("Hello hellow hello");
+})
+
+app.listen(3000 , ()=>{
+    console.log("Server are created a port number 3000");
+}); //assigned an port number , you have created a server on port 3000 , and application is listening on that server
+
+ 
